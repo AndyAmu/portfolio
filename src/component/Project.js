7523e77e3d0303835d4f4ce4f3d1802304a61e59
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-08-11 19:14:05 -0300

    Project styles

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,45 +1,83 @@
 import React from "react"
 import '../index.css'
 import './styles/project.css'
+import LanguageIcon from '@mui/icons-material/Language';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Box } from "@mui/system";
 
 
 const Project = () => {
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }} className="header">
-                <a style={{textDecoration:'none'}} href="https://github.com/AndresAmu/my-tinerary"><div className="card">
-                    <div className="card-info1">
-                    </div>
-                    <p className="title">Ver Repositorio</p>
-                </div>
-                </a>
-                <a style={{textDecoration:'none'}} href="https://github.com/AndresAmu/my-industrial-home">
-                    
-                <div className="card">
-                    <div className="card-info2">
-                    </div>
-                    <p className="title">Ver Repositorio</p>
-                </div>
-                </a>
-                <a style={{textDecoration:'none'}} href="https://github.com/AndresAmu/Amazing_Events">
-                <div className="card">
-                    <div className="card-info3">
-                    
-                    </div>
-                    <p className="title">Ver Repositorio</p>
-                </div>
-                </a>
-                <a style={{textDecoration:'none'}} href="https://github.com/AndresAmu/PETSHOP_MINDY_GRUPO_11">
-                <div className="card">
-                    <div className="card-info4">
-                    </div>
-                    <p className="title">Ver Repositorio</p>
-                </div>
-                </a>
-            </div>
+            <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }} className="header">
+                <a style={{ textDecoration: 'none' }} href="https://github.com/AndresAmu/my-tinerary">
+                    <Box className="card">
+                        <Box className="card-info1">
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <button className="cssbuttons-io">
+                                <span><LanguageIcon /> Web</span>
+                            </button>
 
-                
-            
+                            <button className="cssbuttons-io">
+                                <span><GitHubIcon />Code</span>
+                            </button>
+                        </Box>
+                    </Box>
+                </a>
+                <a style={{ textDecoration: 'none' }} href="https://github.com/AndresAmu/my-industrial-home">
+
+                    <Box className="card">
+                        <Box className="card-info2">
+                        </Box>
+
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <button className="cssbuttons-io">
+                                <span><LanguageIcon /> Web</span>
+                            </button>
+
+                            <button className="cssbuttons-io">
+                                <span><GitHubIcon />Code</span>
+                            </button>
+                        </Box>
+                    </Box>
+
+                </a>
+                <a style={{ textDecoration: 'none' }} href="https://github.com/AndresAmu/Amazing_Events">
+                    <Box className="card">
+                        <Box className="card-info3">
+
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <button className="cssbuttons-io">
+                                <span><LanguageIcon /> Web</span>
+                            </button>
+
+                            <button className="cssbuttons-io">
+                                <span><GitHubIcon />Code</span>
+                            </button>
+                        </Box>
+                    </Box>
+                </a>
+                <a style={{ textDecoration: 'none' }} href="https://github.com/AndresAmu/PETSHOP_MINDY_GRUPO_11">
+                    <Box className="card">
+                        <Box className="card-info4">
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <button className="cssbuttons-io">
+                                <span><LanguageIcon /> Web</span>
+                            </button>
+
+                            <button className="cssbuttons-io">
+                                <span><GitHubIcon />Code</span>
+                            </button>
+                        </Box>
+                    </Box>
+                </a>
+            </Box>
+
+
+
         </>
 
 
